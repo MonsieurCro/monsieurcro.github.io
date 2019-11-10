968c6a3b5e8309cfc76acf4492def665a1ef3094
Cro <MonsieurCro@users.noreply.github.com>
2019-11-10 11:04:25 +0100

    pageLayout

---
+++ scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+  // Progress
+  $(document).scroll(function(){
+    var docHeight = $(document).height();
+    var winHeight = $(window).height();
+    var scrollTop = $(window).scrollTop();
+    var percentage = ((scrollTop / (docHeight - winHeight)) * 100);
+
+    $('.progress').css({'width': percentage + '%'});
+
+    // ScrollTop Toggle
+    if(percentage > 30){
+      $('#topper').show();
+    } else {
+      $('#topper').hide();
+    }
+  });
+
+  // ScrollTop
+  $('#topper').click(function(){
+    $('html').animate({
+      scrollTop: 0
+    }, 'slow');
+  });
+
+});
