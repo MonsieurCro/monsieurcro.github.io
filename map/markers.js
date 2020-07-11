7b38aa16c853de6b1e4a5378bbaed307da310b90
Cro <MonsieurCro@users.noreply.github.com>
2020-07-11 17:28:02 +0200

    json

--- map/markers.js
+++
@@ -1,5 +0,0 @@
-{'markers':[
-    { tags: [], name: 'Binche', coord: '4.165556, 50.410722', image: 'https://live.staticflickr.com/65535/49112033288_e2e25d757b_b.jpg', description: 'Description' },
-    { tags: [], name: '', coord: '', image: '', description: '' },
-    { tags: [], name: '', coord: '', image: '', description: '' }
-]}
