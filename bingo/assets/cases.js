47efdbe4f7357ff6ce5c31112c48ec0fe7e226ca
Cro <MonsieurCro@users.noreply.github.com>
2020-12-16 19:45:25 +0100

    json v0

---
+++ bingo/assets/cases.js
@@ -0,0 +1,14 @@
+var cases = [
+	{
+		"img":"https://img.cuisineaz.com/480x480/2020-09-23/i155229-mercotte.jpeg",
+		"text":"Mercotte dit une crasse sur un gâteau",
+		"step":0,
+		"max":5
+	},
+	{
+		"img":"",
+		"text":"Quand l'entremet n'a pas pris",
+		"step":0,
+		"max":5
+	}
+];
